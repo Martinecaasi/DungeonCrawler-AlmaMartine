58b59c31973344c6689581d3a4e1e07886fa9392
AlmaSchwartz <almaschwartz1@gmail.com>
2025-12-29 12:31:43 +0200

    update monster

--- Monster.cpp
+++ Monster.cpp
@@ -1,43 +1,31 @@
 #include "Monster.hpp"
-#include "Character.hpp"
 
-Monster:: Monster(char* newName,int newHealth,int newStrength, int newDefense)
-          : name(newName),health(newHealth),strength(newStrength),defense(newDefense){};
+Monster::Monster(const std::string& name, int health, int strength, int defense)
+    : name(name), health(health), strength(strength), defense(defense) {}
 
-Monster:: Monster(const Monster& other): name(other.name),health(other.health),strength(other.strength),defense(other.defense){};
-
-Monster& Monster::operator=(const Monster& other){
-          if(this== &other) return *this;
-          delete[]name;
-          name=other.name;
-          health=other.health;
-          strength=other.strength;
-          defense=other.defense;
-
-          return *this;
-};
-
-Monster::~Monster(){
-          delete[] name;
-          name=nullptr;
+const std::string& Monster::getName() const {
+    return name;
 }
 
-
-void Monster:: attack(Character& target){
-          int dmg= strength-target.getDefense();
-          if(dmg<1) dmg=1;
-          target.defend(dmg);
-};
-
-bool Monster::isDefeated() const{
-          return health<=0;
+int Monster::getHealth() const {
+    return health;
 }
 
-char* Monster:: getName()const{ return name;};
-int Monster:: getHealth()const{ return health;};
-int Monster:: getStrength()const{ return strength;};
-int Monster:: getDefense() const{ return defense; };
+int Monster::getStrength() const {
+    return strength;
+}
 
-void Monster::takeDamage(int dmg){
-          health-=dmg;
-};
+int Monster::getDefense() const {
+    return defense;
+}
+
+void Monster::takeDamage(int damage) {
+    if (damage < 0) damage = 0;
+    health -= damage;
+    if (health < 0)
+        health = 0;
+}
+
+bool Monster::isAlive() const {
+    return health > 0;
+}
