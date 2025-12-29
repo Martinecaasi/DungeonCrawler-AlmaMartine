d077456305e7af1866bf9c332a6e5fee8bd4929e
AlmaSchwartz <almaschwartz1@gmail.com>
2025-12-29 12:39:00 +0200

    item.cpp update

--- Item.cpp
+++ Item.cpp
@@ -1,28 +1,23 @@
 #include "Item.hpp"
 
-Item::Item(char* newName,int newHealthB,int newStrengthB,int newDefenseB):
-          name(newName),healthBonus(newHealthB),strengthBonus(newStrengthB),defenseBonus(newDefenseB){}
+Item::Item(const std::string& name, int health, int strength, int defense)
+    : name(name),
+      healthBonus(health),
+      strengthBonus(strength),
+      defenseBonus(defense) {}
 
-Item::Item(const Item& other):name(other.name),healthBonus(other.healthBonus),strengthBonus(other.strengthBonus),defenseBonus(other.defenseBonus){}
-
-Item& Item::operator=(const Item& other){
-          if(this==&other) return *this;
-
-          delete[] name;
-          name=other.name;
-          healthBonus=other.healthBonus;
-          strengthBonus=other.strengthBonus;
-          defenseBonus=other.defenseBonus;
-
-          return *this;
+const std::string& Item::getName() const {
+    return name;
 }
 
-Item::~Item(){
-          delete[] name;
-          name=nullptr;
+int Item::getHealth() const {
+    return healthBonus;
 }
 
-char* Item::getName() const{ return name;};
-int Item::getHealthBonus()const{return healthBonus;};
-int Item:: getStrengthBonus()const{return strengthBonus;};
-int Item:: getDefenseBonus() const{return defenseBonus;};
+int Item::getStrength() const {
+    return strengthBonus;
+}
+
+int Item::getDefense() const {
+    return defenseBonus;
+}
